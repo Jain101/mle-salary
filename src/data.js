f02f60ed82e28735e7a1ec640d35ea3ed2c4b1d5
Jain101 <2021it1ja@mitsgwl.ac.in>
2024-05-20 09:56:41 +0530

    bug #2

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
-export async function fetchData() {
-    const response = await fetch('/salary.json'); // Replace with your file path
-    const data = await response.json(); // Assuming the file is JSON
-    return data;
+// export async function fetchData() {
+//     const response = await fetch('salary.json'); // Replace with your file path
+//     const data = await response.json(); // Assuming the file is JSON
+//     console.log(data);
+//     return data;
+// }
+
+import salary from './salary.json'
+
+export const fetchData = async () => {
+    return salary;
 }
